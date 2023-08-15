86a6d14dbcbc86ced3b4e2b837b51b0ca59e79e3
Josh Hall <joshuarhall0@gmail.com>
2023-08-15 07:46:37 -0700

    Co-authored-by: Kyle Bell  <KyEBell@users.noreply.github.com>

--- client/index.js
+++ client/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import App from './components/App';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import App from "./components/App";
+import { createRoot } from "react-dom/client";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
+    {/* add hash router here?  */}
+    {/* <HashRouter> */}
     <App />
+    {/* </HashRouter> */}
   </React.StrictMode>
 );
