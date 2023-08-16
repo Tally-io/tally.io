c47cfc092cf2061e38ec3fd935435ba7adbcae08
dawit <dawitbogalem@gmail.com>
2023-08-16 10:15:19 -0400

    before merge with morah

--- server/controller/surveyController.js
+++ server/controller/surveyController.js
@@ -1,37 +1,30 @@
 const shortid = require('shortid'); // Import the shortid library
 const surveyModel = require('../model/surveyModel');
 const asyncHandler = require('express-async-handler');
+const { parse } = require('dotenv');
 const { Question, Option } = surveyModel;
 
 const surveyController = {};
 
 surveyController.createQuestion = async (req, res, next) => {
-  console.log('insiede create question');
   try {
     const { title, options, createdBy } = req.body;
-
-    // create the options
-    const optionObjects = options.map(
-      (optionText) => new Option({ option: optionText })
-    );
-
-    // console.log(optionObjects);
-    //save options
-    const savedOptions = await Option.insertMany(optionObjects);
-    console.log('SAVED OPTIONS', savedOptions);
-    // create a unique URL using shortid
-    uniqueUrl = shortid.generate();
 
     //create question
     const newQuestion = new Question({
       title,
-      invitationUrl: uniqueUrl,
       createdBy,
-      options: savedOptions.map((option) => option._id),
+      options: options.map((option) => {
+        return new Option({ text: option });
+      }),
     });
-
+    console.log(newQuestion);
     await newQuestion.save();
-    res.local.createdDoc = newQuestion;
+
+    res.locals.sendDoc = {
+      invitationUrl: `http://localhost:3000/survey/${newQuestion._id}`,
+      id: newQuestion._id,
+    };
     return next();
   } catch (err) {
     return next({
@@ -45,13 +38,8 @@ surveyController.createQuestion = async (req, res, next) => {
 surveyController.serveQuestions = async (req, res, next) => {
   try {
     const questionId = req.params.questionId;
-    const uniqueUrl = req.params.uniqueUrl;
-
-    // Fetch the question using questionId and validate the uniqueUrl
-    const question = await Question.findOne({
-      _id: questionId,
-      invitationUrl: uniqueUrl,
-    }).populate('options');
+    console.log(questionId);
+    const question = await Question.findById(questionId);
 
     if (!question) {
       return res
@@ -59,8 +47,102 @@ surveyController.serveQuestions = async (req, res, next) => {
         .json({ message: 'Question not found or URL invalid.' });
     }
 
-    // Render your survey page with the retrieved question and options
-    res.local.question = question;
+    // send back the entire question using res.locals
+    res.locals.question = question;
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error fetching survey: ${err}`,
+      status: 500,
+      message: 'An error occurred while fetching the survey.',
+    });
+  }
+};
+
+surveyController.selectOption = async (req, res, next) => {
+  try {
+    const { userId, questionId, optionId } = req.body;
+
+    // Find the question
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      return res.status(404).json({ message: 'Question not found' });
+    }
+
+    // Find the selected option within the question
+    const selectedOption = question.options.find(
+      (option) => option._id.toString() === optionId
+    );
+
+    if (!selectedOption) {
+      return res.status(404).json({ message: 'Option not found' });
+    }
+
+    // Check if the user has already selected this option
+    if (selectedOption.selectedBy.includes(userId)) {
+      return res
+        .status(400)
+        .json({ message: 'Option already selected by this user' });
+    }
+
+    // Add the user ID to the selectedBy array
+    selectedOption.selectedBy.push(userId);
+
+    // Update votesCount for the selected option
+    selectedOption.votesCount++;
+
+    // Find the most selected option based on votesCount
+    const mostSelected = question.options.reduce((prev, current) =>
+      prev.votesCount > current.votesCount ? prev : current
+    );
+
+    // Update the mostSelectedOption field in the question
+    question.mostSelectedOption = mostSelected._id;
+
+    // Save the updated question
+    await question.save();
+
+    // Get the selected option text and most selected option text
+    const selectedOptionText = selectedOption.text;
+    const mostSelectedOptionText = mostSelected.text;
+    // console.log('After saving:', question);
+
+    res.locals.options = {
+      selectedOptionText,
+      mostSelectedOptionText,
+    };
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error fetching survey: ${err}`,
+      status: 500,
+      message: 'An error occurred while fetching the survey.',
+    });
+  }
+};
+
+surveyController.getWinner = async (req, res, next) => {
+  try {
+    const questionId = req.params.questionId;
+
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      return res
+        .status(404)
+        .json({ message: 'Question not found or URL invalid.' });
+    }
+
+    // Find the most selected option based on votesCount
+    const mostSelected = question.options.reduce((prev, current) =>
+      prev.votesCount > current.votesCount ? prev : current
+    );
+
+    // send back the  question and winner using res.locals
+    res.locals.question = question.title;
+    res.locals.winner = mostSelected.text;
+
     return next();
   } catch (err) {
     return next({
--- server/router/surveyRouter.js
+++ server/router/surveyRouter.js
@@ -13,18 +13,27 @@ console.log('inside surveyRouter');
 router.post('/createPoll', surveyController.createQuestion, (req, res) => {
   // after middleware, need to send data back to client
   // 201 = created
-  return res.status(201).json(res.locals.createdDoc);
+  return res.status(201).json(res.locals.sendDoc);
 });
 
-router.post(
-  '/survey/:questionId/:uniqueUrl',
-  surveyController.serveQuestions,
+router.get('/:questionId', surveyController.serveQuestions, (req, res) => {
+  return res.status(201).json(res.locals.question);
+});
+
+// Handle user's option selection using PATCH method
+router.patch(
+  '/:questionId/sendOptions',
+  surveyController.selectOption,
   (req, res) => {
-    // after middleware, need to send data back to client
-    // 201 = created
-    return res.status(201).json(res.locals.question);
+    return res.status(201).json(res.locals.options);
   }
 );
+
+router.get('/getWinner/:questionId', surveyController.getWinner, (req, res) => {
+  return res
+    .status(201)
+    .json({ question: res.locals.question, winner: res.locals.winner });
+});
 
 // export
 module.exports = router;
