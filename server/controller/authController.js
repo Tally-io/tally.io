bf48240bb3c9126d34038bda1362c55a857db810
morahgeist <114309093+morahgeist@users.noreply.github.com>
2023-08-15 17:07:31 -0700

    login working no cookies

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,38 +1,38 @@
-const authController = {};
+// const authController = {};
 
-// validate user
-authController.authUser = async (req, res, next) => {
-  console.log('inside authUser in controller');
-  const { email, password } = req.body;
-  // get user from db
-  const userCheck = await User.find({ email });
-  console.log('email : ', email, ' : ', userCheck);
-  // check if user is in db
-  if (userCheck.length <= 0) {
-    return next({ message: { err: 'user not found' } });
-  }
-  //compare password
-  const match = bcrypt.compare(
-    password,
-    userCheck[0].password,
-    function (err, result) {
-      if (result === true) {
-        //console.log('this is the log ', userCheck[0]);
-        res.locals.user = userCheck[0];
-        res.cookie('token', 'logged in');
-        return next();
-      } else {
-        return next({ message: { err: 'user not found or wrong password' } });
-      }
-    }
-  );
-};
+// // validate user
+// authController.authUser = async (req, res, next) => {
+//   console.log('inside authUser in controller');
+//   const { email, password } = req.body;
+//   // get user from db
+//   const userCheck = await User.find({ email });
+//   console.log('email : ', email, ' : ', userCheck);
+//   // check if user is in db
+//   if (userCheck.length <= 0) {
+//     return next({ message: { err: 'user not found' } });
+//   }
+//   //compare password
+//   const match = bcrypt.compare(
+//     password,
+//     userCheck[0].password,
+//     function (err, result) {
+//       if (result === true) {
+//         //console.log('this is the log ', userCheck[0]);
+//         res.locals.user = userCheck[0];
+//         res.cookie('token', 'logged in');
+//         return next();
+//       } else {
+//         return next({ message: { err: 'user not found or wrong password' } });
+//       }
+//     }
+//   );
+// };
 
-authController.validateCookie = (req, res, next) => {
-  if (req.cookies.token === 'logged in') {
-    return next();
-  }
-  res.status(401).send('Plese sign in.');
-};
+// authController.validateCookie = (req, res, next) => {
+//   if (req.cookies.token === 'logged in') {
+//     return next();
+//   }
+//   res.status(401).send('Plese sign in.');
+// };
 
-module.exports = authController;
+// module.exports = authController;
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -38,38 +38,16 @@ userController.registerUser = asyncHandler(async (req, res, next) => {
   return next();
 });
 
-// // validate user
-// userController.authUser = async (req, res, next) => {
-//   // console.log('inside authUser in controller');
-//   const { email, password } = req.body;
-//   // get user from db
-//   const userCheck = await User.find({ email });
-//   console.log('email : ', email, ' : ', userCheck);
-//   // check if user is in db
-//   if (userCheck.length <= 0) {
-//     return next({ message: { err: 'user not found' } });
-//   }
-//   //compare password
-//   const match = bcrypt.compare(
-//     password,
-//     userCheck[0].password,
-//     function (err, result) {
-//       if (result === true) {
-//         //console.log('this is the log ', userCheck[0]);
-//         res.locals.user = userCheck[0];
-//         return next();
-//       } else {
-//         return next({ message: { err: 'user not found or wrong password' } });
-//       }
-//     }
-//   );
-// };
-
-// Delete a user
-// email and password must be passed in request body
-userController.deleteUser = async (req, res, next) => {
+// validate user
+userController.authUser = async (req, res, next) => {
   const { email, password } = req.body;
-  //console.log('entering deletion middleware ');
+  // get user from db
+  const userCheck = await User.find({ email });
+  console.log('email : ', email, ' : ', userCheck);
+  // check if user is in db
+  if (userCheck.length <= 0) {
+    return next({ message: { err: 'user not found' } });
+  }
   //compare password
   const match = bcrypt.compare(
     password,
@@ -84,6 +62,13 @@ userController.deleteUser = async (req, res, next) => {
       }
     }
   );
+};
+
+// Delete a user
+// email and password must be passed in request body
+userController.deleteUser = async (req, res, next) => {
+  const { email } = req.body;
+  //console.log('entering deletion middleware ');
 
   try {
     // find the user by email and delete
